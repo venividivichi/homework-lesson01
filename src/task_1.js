2c193a8898cd6c5761c134a15fb34a51c4eed7e6
Nazarii <moloduy23@gmail.com>
2023-09-27 16:34:42 +0300

    My Homework 11

--- src/task_1.js
+++ src/task_1.js
@@ -1,53 +1,19 @@
-const notification = {
-    radio: [{
-      source: 'radio',
-      text: 'Загроза застосування балістичного озброєння!',
-      date: '01.08.2023'
-    }],
-    sms: [{
-      source: 'sms',
-      text: 'Загроза застосування хімічної зброї!',
-      date: '10.08.2023'
-    }],
-    email: [{
-      source: 'email',
-      text: 'Загроза застосування ракетного озброєння!',
-      date: '30.08.2023'
-    }],
-    ZASCO: [{
-      source: 'ZASCO',
-      text: 'Загроза застосування ядерної зброї!',
-      date: '30.08.2023'
-    }],
-    [Symbol.iterator]() {
-        let valueIndex = 0;
-        let inerIndex = 0;
-        const values = [];
 
-        for (const key in this) {
-            values.push(this[key]);
-        }
+function logArguments(fn) {
+  
+  return function (...args) {
+   
+    console.log('Arguments: ', args);
 
-        return {
-
-            next() {
-
-              if (inerIndex === values[valueIndex].length) {
-                inerIndex = 0;
-                valueIndex++;
-              }
-
-              if (valueIndex === values.length) { return {done: true} }
-
-              return {
-                value: values[valueIndex][inerIndex++],
-                done: false
-              }   
-            }
-        }
-    }
+    return fn(...args)
+  }
+  
 }
 
-for (const item of notification) {
-  console.log(item)
+function sumCalc(a, b) {
+  return a + b
 }
+
+let tryDecorate = logArguments(sumCalc);
+
+console.log('Result: ', tryDecorate(2, 2));
