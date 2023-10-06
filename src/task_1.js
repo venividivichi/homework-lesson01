426a21114efe8b4fa4a22663a6f142a32bd8601d
Nazarii <moloduy23@gmail.com>
2023-10-06 11:03:13 +0300

    Homework 12

--- src/task_1.js
+++ src/task_1.js
@@ -1,67 +1,14 @@
 
-function Student (name, surname, birthday, course) {
-  this.name = name;
-  this.surname = surname;
-  this.birthday = birthday;
-  this.marks = [];
-  this.attendance = [];
-  this.course = course
-}
+function summarize(num) {
 
-Student.prototype.addMark = function (mark) {
-  this.marks.push(mark) 
-}
+  if (typeof num != 'number')
+    num = 1;
 
-Student.prototype.addAttendance = function (isPres) {
-  this.attendance.push(isPres)
-} 
-
-Student.prototype.getAvgSuccessRate = function () {
-  let sumMark = 0;
-  
-  for (item of this.marks) {
-    sumMark += item 
+  return function(n) {
+    return n + num
   }
-  return sumMark / this.marks.length
 }
 
-Student.prototype.getAvgAttendance = function () {
-  let presentCount = this.attendance.filter(isPresent => isPresent)
-  return (presentCount.length / this.attendance.length) * 100
-}
+let summaNumb = summarize(5)
 
-Student.prototype.getCountAttendances = function () {
-  return this.attendance.length
-}
-
-Student.prototype.changeCourse = function (newCourse) {
-  this.course = newCourse
-}
-
-Student.prototype.getAllInfo = function () {
- 
-  return `
-    Ім'я: ${this.name}
-    Прізвище: ${this.surname}
-    Рік народження: ${this.birthday}
-    Назва курсу: ${this.course}
-    Отримані оцінки: ${this.marks.join(', ')}, успішність: ${this.getAvgSuccessRate()}%
-    Пройдених занять: ${this.getCountAttendances()}
-    Середня відвідуваність: ${this.getAvgAttendance()}%
-    `
-}
-
-const student_L = new Student('Любомир', 'Любомирович', 1994, 'Математика');
-
-student_L.addMark(87);
-student_L.addMark(97);
-student_L.addAttendance(true);
-student_L.addAttendance(false);
-student_L.addAttendance(true);
-student_L.addAttendance(false);
-
-console.log('Середня успішність: ' + student_L.getAvgSuccessRate());
-console.log('Середня відвідуваність: ' + student_L.getAvgAttendance() + '%');
-console.log('Кількість пройдених занять: ' + student_L.getCountAttendances());
-console.log('-----------------------------')
-console.log('інформація про студента: ' + student_L.getAllInfo());
+console.log(summaNumb(3));
--- src/task_2.js
+++ src/task_2.js
@@ -1,113 +1,29 @@
 
-function Student (firstName, lastName, birthday) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthday = birthday;
-  this.marks = {};
-  this.attendance = {};
-  this.courses = [];
-}
+function counter(startValue, step) {
 
-Student.prototype.addMark = function (mark, course) {
+  let startNumb = startValue;
 
-  if (!Object.hasOwn(this.marks, course)) {
-    this.marks[course] = [];
-  }
-  
-  this.marks[course].push(mark);
-}
-
-Student.prototype.addAttendance = function (isPres, course) {
-  
-  if (!Object.hasOwn(this.attendance, course)) {
-    this.attendance[course] = [];
+  function increment() { 
+    return  startNumb += step;
   }
 
-  this.attendance[course].push(isPres)
-} 
-
-Student.prototype.getAvgSuccessRate = function (course) {
-  let sumMark = 0;
-      
-  for (item of this.marks[course]) {
-      sumMark += item 
-  } 
-
-  return sumMark / this.marks[course].length
-}
-
-Student.prototype.getAvgAttendance = function (course) {
-  let presentCount = this.attendance[course].filter(isPresent => isPresent)
-  return (presentCount.length / this.attendance[course].length) * 100
-}
-
-Student.prototype.getCountAttendances = function (course) {
-  if (this.attendance[course])
-    return this.attendance[course].length;
-}
-
-Student.prototype.changeCourse = function (newCourse) {
-  this.courses.push(newCourse);
-}
-
-Student.prototype.removeCourse = function (courseRmv) {
-  
-  let index = this.courses.indexOf(courseRmv);
-  
-  if (index !== -1) {
-    this.courses.splice(index, 1);
+  function decrement() {
+    return startNumb -= step;
   }
 
-  delete this.marks[courseRmv];
-  delete this.attendance[courseRmv];
+  function reset() {
+    return startNumb = startValue;
+  }
+
+  return { 
+  //  increment() { return  startNumb += step;}, 
+    increment,
+    decrement, reset }
+
 }
 
-function getCourseInfo(student) {
- 
-  return student.courses.map(course => {
-   
-    return `
-    Course name: ${course}
-    Get marks: ${student.marks[course].join(', ')}; average success rate: ${student.getAvgSuccessRate(course)}%
-    Count attendances: ${student.getCountAttendances(course)}
-    Average attendances: ${student.getAvgAttendance(course).toFixed(2)}%
-    `
-  }).join('\n');
-}
+const stepper = counter(0, 2)
 
-Student.prototype.getAllInfo = function () {
-
-  return `
-    Firstname: ${this.firstName}
-    Lastname: ${this.lastName}
-    Birthday: ${this.birthday}
-  
-    Course:
-    ${getCourseInfo(this)}
-  `
-}
-
-const student_L = new Student('Любомир', 'Любомирович', 1994);
-
-student_L.changeCourse('Math');
-student_L.changeCourse('English');
-
-student_L.addMark(87, 'Math');
-student_L.addMark(97, 'English');
-student_L.addMark(70, 'Math');
-
-student_L.addAttendance(true, 'Math');
-student_L.addAttendance(false, 'Math');
-student_L.addAttendance(false, 'Math');
-student_L.addAttendance(true, 'English');
-student_L.addAttendance(false, 'English');
-
-//student_L.removeCourse('bla2') - якщо треба видалити курс
-
-//  окремо методи студента 
-//  console.log('Середня успішність: ' + student_L.getAvgSuccessRate('bla1'));
-//  console.log('Середня відвідуваність: ' + student_L.getAvgAttendance('bla1').toFixed(2) + '%');
-//  console.log('Кількість пройдених занять: ' + student_L.getCountAttendances('bla1'));
- 
-console.log('-----------------------------');
-console.log('Info about student: ' + student_L.getAllInfo());
+console.log(stepper.increment());
+console.log(stepper.increment());
+console.log(stepper.decrement());
