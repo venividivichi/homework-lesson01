ebf4d0033153f02ce33a56965ab46a00cab470bd
Nazarii <moloduy23@gmail.com>
2023-10-09 17:44:50 +0300

    Homework 14

--- src/task_1.js
+++ src/task_1.js
@@ -1,14 +1,111 @@
 
-function summarize(num) {
-
-  if (typeof num != 'number')
-    num = 1;
-
-  return function(n) {
-    return n + num
+class TodoList {
+  constructor() {
+    this.notes = [];
   }
+
+  addNote(textNote) {
+    if (textNote.trim() !== '') {
+      const note = {
+        note_cmplt_st: false,
+        text: textNote
+      };
+      this.notes.push(note);
+    } else {
+      console.log('Note can`t be empty!');
+    }
+  }
+
+  deleteNote(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes.splice(index, 1);
+    } else {
+      console.log('Note with index ' + index +  ' not found!');
+    }
+  }
+
+  editNote(newStatus, newNoteText, index) {
+    if (index >= 0 && index < this.notes.length) {
+     
+      if (newNoteText.trim() != '') {
+        this.notes[index].text = newNoteText;
+      } else {
+        console.log('Note can`t be empty!')
+      }
+
+      if (typeof newStatus === 'boolean') {
+        this.notes[index].note_cmplt_st = newStatus;
+      } 
+
+    } else {
+      console.log('Error with edit note! Note with index ' + index + ' not found!');
+    }
+  }
+
+  getNoteAllInfo(index) {
+    if (index >= 0 && index < this.notes.length) {
+      return `
+        Note status: ${this.notes[index].note_cmplt_st}
+        Task: ${this.notes[index].text}
+      `
+    } else {
+      console.log('Note with index ' + index + ' not found!');
+    }
+  }
+
+  getAllNotes() {
+    return this.notes;
+  }
+
+  getAllNotesByList() {
+  if (this.notes.length === 0) {
+      console.log('Notes list empty.');
+    } else {
+      console.log('Notes list:');
+        this.notes.forEach((note, index) => {
+        console.log(`${index + 1}. Status: ${note.note_cmplt_st}, task: ${note.text}`);
+      });
+    }
+  }
+
+  getCountOfAllNotes() {
+    return this.notes.length;
+  }
+
+  getCountOfUncompletedNotes() {
+    return this.notes.filter((note) => !note.note_cmplt_st).length;
+  }
+
+  markNoteAsCompleted(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes[index].note_cmplt_st = true;
+    } else {
+      console.log('Note with index ' + index + ' not found!');
+    }
+  }
+
 }
 
-let summaNumb = summarize(5)
+let myTodoList = new TodoList();
 
-console.log(summaNumb(3));
+myTodoList.addNote('Wash my face');
+myTodoList.addNote('Make breakfast');
+myTodoList.addNote('Walk the dog'); 
+myTodoList.addNote('Some to change ...'); 
+myTodoList.addNote('Take a shower'); 
+
+console.log('Note info: ', myTodoList.getNoteAllInfo(2));
+
+console.log('All notes (like Object): ', myTodoList.getAllNotes());
+console.log('All notes count: ' + myTodoList.getCountOfAllNotes())
+console.log('Count of unfinished notes: ' + myTodoList.getCountOfUncompletedNotes())
+
+myTodoList.editNote(true, 'Lie down few minutes', 3);
+
+//myTodoList.deleteNote(3);
+
+myTodoList.markNoteAsCompleted(0);
+console.log('--------- after changes ---------');
+console.log('Count of unfinished notes: ' + myTodoList.getCountOfUncompletedNotes())
+console.log('------------------');
+console.log(myTodoList.getAllNotesByList())
--- src/task_2.js
+++ src/task_2.js
@@ -1,29 +1,137 @@
 
-function counter(startValue, step) {
-
-  let startNumb = startValue;
-
-  function increment() { 
-    return  startNumb += step;
+class TodoList {
+  constructor() {
+    this.notes = [];
   }
 
-  function decrement() {
-    return startNumb -= step;
+  addNote(textNote) {
+    if (textNote.trim() !== '') {
+      const note = {
+        note_cmplt_st: false,
+        text: textNote
+      };
+      this.notes.push(note);
+    } else {
+      console.log('Note can`t be empty!');
+    }
   }
 
-  function reset() {
-    return startNumb = startValue;
+  deleteNote(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes.splice(index, 1);
+    } else {
+      console.log('Note with index ' + index + ' not found!');
+    }
   }
 
-  return { 
-  //  increment() { return  startNumb += step;}, 
-    increment,
-    decrement, reset }
+  editNote(newStatus, newNoteText, index) {
+    if (index >= 0 && index < this.notes.length) {
+     
+      if (newNoteText.trim() != '') {
+        this.notes[index].text = newNoteText;
+      } else {
+        console.log('Note can`t be empty!')
+      }
+
+      if (typeof newStatus === 'boolean') {
+        this.notes[index].note_cmplt_st = newStatus;
+      } 
+
+    } else {
+      console.log('Error with edit note! Note with index ' + index + ' not found!');
+    }
+  }
+
+  getNoteAllInfo(index) {
+    if (index >= 0 && index < this.notes.length) {
+      return `
+        Note status: ${this.notes[index].note_cmplt_st}
+        Task: ${this.notes[index].text}
+      `
+    } else {
+      console.log('Note with index ' + index + ' not found!');
+    }
+  }
+
+  getAllNotes() {
+    return this.notes;
+  }
+
+  getAllNotesByList() {
+  if (this.notes.length === 0) {
+      console.log('Notes list empty.');
+    } else {
+      console.log('Notes list:');
+        this.notes.forEach((note, index) => {
+        console.log(`${index + 1}. Status: ${note.note_cmplt_st}, task: ${note.text}`);
+      });
+    }
+  }
+
+  getCountOfAllNotes() {
+    return this.notes.length;
+  }
+
+  getCountOfUncompletedNotes() {
+    return this.notes.filter((note) => !note.note_cmplt_st).length;
+  }
+
+  markNoteAsCompleted(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes[index].note_cmplt_st = true;
+    } else {
+      console.log('Note with index ' + index + ' not found!');
+    }
+  }
+
+  getNoteByName(searchText) {
+    this.notes.forEach((note, index) => {
+      if(note.text === searchText) {
+      console.log(`${index}. Status: ${note.note_cmplt_st}, task: ${note.text}`);
+      } 
+    });
+  }
+
+  sortNotesByStatus(sortParam = true) {
+    this.notes.sort( (a, b) => {
+      if (a.note_cmplt_st === b.note_cmplt_st) {
+        return 0;
+      }
+      if (sortParam) {
+        return b.note_cmplt_st
+      } else {
+        return a.note_cmplt_st
+      }
+    });
+  }
 
 }
 
-const stepper = counter(0, 2)
+let myTodoList = new TodoList();
 
-console.log(stepper.increment());
-console.log(stepper.increment());
-console.log(stepper.decrement());
+myTodoList.addNote('Wash my face');
+myTodoList.addNote('Make breakfast');
+myTodoList.addNote('Walk the dog'); 
+myTodoList.addNote('Some to change ...'); 
+myTodoList.addNote('Take a shower'); 
+
+console.log('Note info: ', myTodoList.getNoteAllInfo(2));
+
+console.log('All notes (like Object): ', myTodoList.getAllNotes());
+console.log('All notes count: ' + myTodoList.getCountOfAllNotes())
+console.log('Count of unfinished notes: ' + myTodoList.getCountOfUncompletedNotes())
+
+myTodoList.editNote(true, 'Lie down few minutes', 3);
+
+//myTodoList.deleteNote(3);
+
+myTodoList.markNoteAsCompleted(0);
+console.log('--------- after changes ---------');
+console.log('Count of unfinished notes: ' + myTodoList.getCountOfUncompletedNotes())
+console.log('------------------');
+console.log(myTodoList.getAllNotesByList())
+console.log('--------- find ---------');
+console.log(myTodoList.getNoteByName('Walk the dog'))
+console.log('--------- sort ---------');
+console.log(myTodoList.sortNotesByStatus(false)); 
+console.log(myTodoList.getAllNotesByList())
