0d712db210111581686a6d95252d48014bef15c6
Nazarii <moloduy23@gmail.com>
2023-10-17 12:03:10 +0300

    Homework 15

--- src/task_1.js
+++ src/task_1.js
@@ -1,111 +1,37 @@
-
-class TodoList {
-  constructor() {
-    this.notes = [];
-  }
-
-  addNote(textNote) {
-    if (textNote.trim() != '') {
-      const note = {
-        note_cmplt_st: false,
-        text: textNote
-      };
-      this.notes.push(note);
-    } else {
-      console.log('Note can`t be empty!');
-    }
-  }
-
-  deleteNote(index) {
-    if (index >= 0 && index < this.notes.length) {
-      this.notes.splice(index, 1);
-    } else {
-      console.log('Note with index ' + index +  ' not found!');
-    }
-  }
-
-  editNote(newStatus, newNoteText, index) {
-    if (index >= 0 && index < this.notes.length) {
-     
-      if (newNoteText.trim() != '') {
-        this.notes[index].text = newNoteText;
-      } else {
-        console.log('Note can`t be empty!')
-      }
-
-      if (typeof newStatus === 'boolean') {
-        this.notes[index].note_cmplt_st = newStatus;
-      } 
-
-    } else {
-      console.log('Error with edit note! Note with index ' + index + ' not found!');
-    }
-  }
-
-  getNoteAllInfo(index) {
-    if (index >= 0 && index < this.notes.length) {
-      return `
-        Note status: ${this.notes[index].note_cmplt_st}
-        Task: ${this.notes[index].text}
-      `
-    } else {
-      console.log('Note with index ' + index + ' not found!');
-    }
-  }
-
-  getAllNotes() {
-    return this.notes;
-  }
-
-  getAllNotesByList() {
-  if (this.notes.length === 0) {
-      console.log('Notes list empty.');
-    } else {
-      console.log('Notes list:');
-        this.notes.forEach((note, index) => {
-        console.log(`${index}. Status: ${note.note_cmplt_st}, task: ${note.text}`);
-      });
-    }
-  }
-
-  getCountOfAllNotes() {
-    return this.notes.length;
-  }
-
-  getCountOfUncompletedNotes() {
-    return this.notes.filter((note) => !note.note_cmplt_st).length;
-  }
-
-  markNoteAsCompleted(index) {
-    if (index >= 0 && index < this.notes.length) {
-      this.notes[index].note_cmplt_st = true;
-    } else {
-      console.log('Note with index ' + index + ' not found!');
-    }
-  }
-
+function isPalindrome(number) {
+  let numString = number.toString();
+  let reversedNum = numString.split("").reverse().join("");
+  
+  return numString === reversedNum;
 }
 
-let myTodoList = new TodoList();
+function findLychrelNumber(startNumber) {
+  let currentNumber = startNumber;
+  let steps = 0;
 
-myTodoList.addNote('Wash my face');
-myTodoList.addNote('Make breakfast');
-myTodoList.addNote('Walk the dog'); 
-myTodoList.addNote('Some to change ...'); 
-myTodoList.addNote('Take a shower'); 
+  while (steps < 1000) { 
+    let sum = 0;
+    let reversedNumber = parseInt(currentNumber.toString().split("").reverse().join(""));
+    //console.log(reversedNumber);
+    sum = currentNumber + reversedNumber;
 
-console.log('Note info: ', myTodoList.getNoteAllInfo(2));
+    currentNumber = sum;
+    steps++;
 
-console.log('All notes (like Object): ', myTodoList.getAllNotes());
-console.log('All notes count: ' + myTodoList.getCountOfAllNotes())
-console.log('Count of unfinished notes: ' + myTodoList.getCountOfUncompletedNotes())
+    if (isPalindrome(sum)) {
+      return { result: sum, steps };
+    }
+  }
 
-myTodoList.editNote(true, 'Lie down few minutes', 3);
+  return { result: 'not found', steps };
+}
 
-//myTodoList.deleteNote(3);
+let lychrelNumber = 312;
+let findResult = findLychrelNumber(lychrelNumber);
 
-myTodoList.markNoteAsCompleted(0);
-console.log('--------- after changes ---------');
-console.log('Count of unfinished notes: ' + myTodoList.getCountOfUncompletedNotes())
-console.log('------------------');
-console.log(myTodoList.getAllNotesByList())
+if (findResult.result !== 'not found') {
+  console.log(`Palindrome for number ${lychrelNumber} find: is ${findResult.result} in ${findResult.steps} steps.`);
+} else {
+  console.log(`Palindrome for number ${lychrelNumber} don\`t found after ${findResult.steps} steps.`);
+}
+
